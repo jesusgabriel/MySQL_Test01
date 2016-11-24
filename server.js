0676a83bf8b1a12958cdb57a3b0f4cdcb3825dac
jesusgabriel <amelo@fvi.edu>
2016-11-23 21:08:35 -0500

    save

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const PORT = 3001;
+
+app.get('/', function(req, res){
+  res.send("hello word")
+});
+app.listen(PORT, function(){
+  console.log("server listening");
+});
